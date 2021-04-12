4a9e0e1e4374f294659486ac984c1f61f4354c4d
arpitkalla <kalla.arpit@gmail.com>
2021-04-12 12:59:10 -0400

    Adding SO data analysis and plots

--- data/so_analyze.py
+++ data/so_analyze.py
@@ -1,7 +1,7 @@
 import argparse
 
 import pandas as pd
-
+import matplotlib.pyplot as plt 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", "-i", default="./data/so_python_10000.csv", type=str, help="Path to the csv file")
 
@@ -11,6 +11,18 @@ def count_accepted_answers(df):
     df = df.drop_duplicates('q_id')
     count = sum(df["q_accepted_answer_id"].notna())
     print(f"Number of questions w/ accepted answers: {count} / {len(df)}")
+
+# Plot the histogram of answer count for questions
+def hist_answer_count(df):
+    df = df.drop_duplicates('q_id')
+    vc = df["q_answer_count"].value_counts()
+    median = df["q_answer_count"].median()
+    print(f"Median number of answers to questions: {median}")
+    vc.plot(kind='bar')
+    plt.title("Count of different Answers to a Question on StackOverflow")
+    plt.xlabel("Count of Questions")
+    plt.ylabel("Count of different Answers")
+    plt.savefig("data/plots/so_hist_count_answers_to_questions.png")
 
 # Count the number of answers with code sample
 def count_answers_w_code(df):
@@ -23,6 +35,7 @@ def main():
     df = pd.read_csv(args.input)
     count_accepted_answers(df)
     count_answers_w_code(df)
+    hist_answer_count(df)
 
 if __name__ == "__main__":
     main()
