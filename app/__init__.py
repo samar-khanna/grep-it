8091fc0dc85822a0f8086032ea3072d9832639bd
Arpit Kalla <kalla.arpit@gmail.com>
2021-04-18 16:46:32 -0400

    Frontend (#2)
    * Init
    
    * Fixing input issue
    
    * Adding animations
    
    * Editing styles
    
    * Serving react app
    
    * Adding root package.json for heroku
    
    Co-authored-by: Luca Koval <lucakoval@me.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,24 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, send_from_directory, render_template, request
 from .irsystem.models.search import *
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build/static', template_folder="../frontend/build")
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-#controller for now
-@app.route('/search', methods=['GET'])
-def search():
-	query = request.args.get('search')
-	if not query:
-	    results = []
-	    query_confirmation = ''
-	else:
-		query_confirmation = "Your search: " + query
-		results = test(query) #top results from search.py
-	return render_template('index.html', query_confirmation=query_confirmation, results=results)
+# @app.route('/<path:path>')
+# def serve_page(path):
+#     return send_from_directory('../frontend/build/static', path)
+
+# #controller for now
+# @app.route('/search', methods=['GET'])
+# def search():
+# 	query = request.args.get('search')
+# 	if not query:
+# 	    results = []
+# 	    query_confirmation = ''
+# 	else:
+# 		query_confirmation = "Your search: " + query
+# 		results = test(query) #top results from search.py
+# 	return render_template('index.html', query_confirmation=query_confirmation, results=results)
---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import './styles/general.css';
+import styles from './styles/App.module.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      textInputFocused: false
+    }
+  }
+
+  onTextInputBlur = (e) => {
+    this.setState({ textInputFocused: false })
+  }
+
+  onTextInputFocus = (e) => {
+    this.setState({ textInputFocused: true })
+  }
+
+  render() {
+    let borderStyle = this.state.textInputFocused ? { border: "2px solid var(--green)" } : {}
+    return (
+      <div className={styles.container}>
+        <div className={styles.centralCol}>
+          <div className={styles.title}>Grep It</div>
+          <div style={borderStyle} className={styles.textInputContainer}>
+            <input
+              type="text"
+              className={styles.textInput}
+              onBlur={this.onTextInputBlur}
+              onFocus={this.onTextInputFocus}
+              placeholder="How to add async..."
+            />
+          </div>
+          <textarea className={styles.codeInput} />
+          <div className={styles.buttonContainer}>
+            <input type="Submit" className={styles.button} value="Submit" />
+            <div className={styles.buttonShadow} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
