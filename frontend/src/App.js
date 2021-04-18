68294688ca7566e7c9dfa236f3c6b9cd9e8d73ab
Luca Koval <lucakoval@me.com>
2021-04-18 14:58:31 -0400

    Fixing input issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/general.css';
+import styles from './styles/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.centralCol}>
+        <div className={styles.logo}></div>
+        <div className={styles.title}>Grep It</div>
+        <div className={styles.textInputContainer}>
+          <input type="search" placeholder="How to add async..."/>
+        </div>
+        <div className={styles.codeInputContainer}></div>
+        <div className={styles.buttonContainer}></div>
+      </div>
     </div>
   );
 }
