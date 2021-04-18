d156390a95d96c2a7b99a3a29a5a07ad639fac46
Luca Koval <lucakoval@me.com>
2021-04-18 15:30:15 -0400

    Adding animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,50 @@
+import React, { Component } from 'react';
+
 import './styles/general.css';
 import styles from './styles/App.module.css';
 
-function App() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.centralCol}>
-        <div className={styles.logo}></div>
-        <div className={styles.title}>Grep It</div>
-        <div className={styles.textInputContainer}>
-          <input type="search" placeholder="How to add async..."/>
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      textInputFocused: false
+    }
+  }
+
+  onTextInputBlur = (e) => {
+    this.setState({ textInputFocused: false })
+  }
+
+  onTextInputFocus = (e) => {
+    this.setState({ textInputFocused: true })
+  }
+
+  render() {
+    let borderStyle = this.state.textInputFocused ? { border: "2px solid var(--green)" } : {}
+    return (
+      <div className={styles.container}>
+        <div className={styles.centralCol}>
+          <div className={styles.logo}></div>
+          <div className={styles.title}>Grep It</div>
+          <div style={borderStyle} className={styles.textInputContainer}>
+            <input
+              type="text"
+              className={styles.textInput}
+              onBlur={this.onTextInputBlur}
+              onFocus={this.onTextInputFocus}
+              placeholder="How to add async..."
+            />
+          </div>
+          <textarea className={styles.codeInput} />
+          <div className={styles.buttonContainer}>
+            <input type="Submit" className={styles.button} value="Submit" />
+            <div className={styles.buttonShadow} />
+          </div>
         </div>
-        <div className={styles.codeInputContainer}></div>
-        <div className={styles.buttonContainer}></div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
