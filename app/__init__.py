416cf88c126ac063e494f9c4ce28545b4869c30f
Samar Khanna <samar.khanna@gmail.com>
2021-04-18 16:23:12 -0400

    added cosine search over answer tokens

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,35 @@
 import os
 from flask import Flask, render_template, request
 from .irsystem.models.search import *
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-#controller for now
+
+# controller for now
 @app.route('/search', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-	    results = []
-	    query_confirmation = ''
-	else:
-		query_confirmation = "Your search: " + query
-		results = jaccard_search(query) #top results from search.py
-	return render_template('index.html', query_confirmation=query_confirmation, results=results)
+    query = request.args.get('search')
+    if not query:
+        results = []
+        query_confirmation = ''
+    else:
+        query_confirmation = "Your search: " + query
+        results = jaccard_search(query)  # top results from search.py
+    return render_template('index.html', query_confirmation=query_confirmation, results=results)
 
 
 @app.route('/search_cosine', methods=['GET'])
 def cosine_search():
-	query = request.args.get('search')
-	if not query:
-	    results = []
-	    query_confirmation = ''
-	else:
-		query_confirmation = "Your search: " + query
-		results = cosine_sim(query) #top results from search.py
-	return render_template('index.html', query_confirmation=query_confirmation, results=results)
+    query = request.args.get('search')
+    if not query:
+        results = []
+        query_confirmation = ''
+    else:
+        query_confirmation = "Your search: " + query
+        results = cosine_sim(query)  # top results from search.py
+    return render_template('index.html', query_confirmation=query_confirmation, results=results)
--- app/irsystem/models/init_cosine.py
+++ app/irsystem/models/init_cosine.py
@@ -1,32 +1,37 @@
+import ast
 import pandas as pd
-import numpy as np
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
+from tokenizers import Tokenizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def cosine_data():
+def get_tfidf():
     print("Preprocessing cosine data....")
-    #df = pd.read_csv ('../../../data/so_rust.csv')
-    df = pd.read_csv ('./data/so_rust.csv')
-    titles = df['q_title'].to_dict()
-    qidToQuestion = dict(zip(df.q_id, df.q_title)) #for each question, maps qid => actual question query
+    df = pd.read_csv('./data/so_rust_lang_code.csv')
+    language_tokenizer = Tokenizer.from_file('./data/language-tokenizer.json')
+    # code_tokenizer = Tokenizer.from_file('./data/rust-tokenizer.json')
 
-    # get train/test set from data (question titles for now)
-    docs = list(titles.values())
-    train = docs[:len(docs)-10]
-    test = docs[len(docs)-10:]
+    # q_body_tokens = df['q_body_tokens']
+    a_body_tokens = df['a_body_tokens']
 
-    #maps index of question title (0,1,2...) to actual question id
-    idxToQid = dict(zip([i for i in range(len(docs))], df.q_id)) #idx of question title (0,1,..) => actual question id
+    # TODO: Refactor this by saving tokens in correct format
+    def tokenize(inp_str):
+        return ' '.join(ast.literal_eval(inp_str))
 
-    #creates tf-idf
-    tfIdfVectorizer=TfidfVectorizer(use_idf=True)
-    tfidf = tfIdfVectorizer.fit_transform(docs) #using all docs for now
-    similarity_matrix = cosine_similarity(tfidf, tfidf)
+    # q_body_tokens = q_body_tokens.apply(tokenize)
+    a_body_tokens = a_body_tokens.apply(tokenize)
 
-    return tfidf, tfIdfVectorizer, qidToQuestion, idxToQid #create once
+    a_vectorizer = TfidfVectorizer(analyzer=str.split)
+    a_tfidf = a_vectorizer.fit_transform(a_body_tokens)
 
-#sets global variables so we don't have to recalculate
-tfidf, tfIdfVectorizer, qidToQuestion, idxToQid = cosine_data()
+    # for each question, maps qid => actual question query
+    qid_to_question = dict(zip(df.q_id, df.q_title))
 
+    # maps index of question title (0,1,2...) => actual question id
+    idx_to_qid = dict(zip([i for i in range(len(df))], df.q_id))
+
+    return language_tokenizer, a_vectorizer, a_tfidf, qid_to_question, idx_to_qid
+
+
+# sets global variables so we don't have to recalculate
+language_tokenizer, a_vectorizer, a_tfidf, qid_to_question, idx_to_qid = get_tfidf()
