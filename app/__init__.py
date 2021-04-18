fc40497467a48000928937926b65b36fa6638a59
arpitkalla <kalla.arpit@gmail.com>
2021-04-18 17:00:36 -0400

    Adding search route for testing

--- app/__init__.py
+++ app/__init__.py
@@ -7,18 +7,12 @@ app = Flask(__name__, static_folder='../frontend/build/static', template_folder=
 def index():
     return render_template("index.html")
 
-# @app.route('/<path:path>')
-# def serve_page(path):
-#     return send_from_directory('../frontend/build/static', path)
-
-# #controller for now
-# @app.route('/search', methods=['GET'])
-# def search():
-# 	query = request.args.get('search')
-# 	if not query:
-# 	    results = []
-# 	    query_confirmation = ''
-# 	else:
-# 		query_confirmation = "Your search: " + query
-# 		results = test(query) #top results from search.py
-# 	return render_template('index.html', query_confirmation=query_confirmation, results=results)
+@app.route('/search', methods=['GET'])
+def search():
+	query = request.args.get('search')
+	if not query:
+	    results = []
+	    query_confirmation = ''
+	else:
+		query_confirmation = "Your search: " + query
+	return {"query": query_confirmation}, 200
