e059624771cf7c750ee2e7aadb1a3ef1a49375a1
Arpit Kalla <kalla.arpit@gmail.com>
2021-04-19 01:39:36 -0400

    Update frontend (#4)
    * Init
    
    * Fixing input issue
    
    * set up initial cosine_sim using question titles
    
    * Adding animations
    
    * Editing styles
    
    * added cosine search over answer tokens
    
    * New requirements
    
    * Serving react app
    
    * Adding root package.json for heroku
    
    * Adding functions
    
    * Fixing CORS
    
    * Trying request
    
    * From master
    
    * Scrolling
    
    * Adding textarea dynamic height
    
    * Adding key
    
    * Adding random queries as examples
    
    Co-authored-by: Luca Koval <lucakoval@me.com>
    Co-authored-by: luOlivia <olivialu46@gmail.com>
    Co-authored-by: Samar Khanna <samar.khanna@gmail.com>

---
+++ frontend/src/constants/Queries.js
@@ -0,0 +1,11 @@
+var queries = [
+    {
+        "text": "String to str",
+        "code": "String s = String::new(\"hello\"));"
+    },
+    {
+        "text": "How to use async method with tokio",
+        "code": "#async![tokio]\npub fn main {\n}"
+    }
+]
+export default queries;
