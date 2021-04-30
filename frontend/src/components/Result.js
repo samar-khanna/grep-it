0dc1ab21b14aef99b8fecce00036dfcc293cb5ec
Luca Koval <lucakoval@me.com>
2021-04-29 23:23:32 -0400

    Fixing styling

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -10,17 +10,18 @@ class Result extends Component {
     render() {
         return (
             <div className={styles.container}>
+                <div className={styles.questionContainer}>
+                    <div className="bold">Question:&nbsp;</div>
+                    <a href={this.props.url}>{this.props.title}</a>
+                </div>
                 <div className={styles.answerContainer}>
+                    <div className="bold">Answer:</div>
                     <div
                         className={styles.answer}
                         dangerouslySetInnerHTML={{
                             __html: this.props.answer
                         }}
                     />
-                </div>
-                <div className={styles.urlContainer}>
-                    <div className="bold">Link to post:&nbsp;</div>
-                    <a href={this.props.url}>{this.props.title}</a>
                 </div>
             </div>
         )
