a61dc46314b4405664100d8d3583e23ca95c7f09
Samar Khanna <samar.khanna@gmail.com>
2021-04-30 23:25:24 -0400

    Updated to use non conflicting var names

--- app/irsystem/models/init_tfidf.py
+++ app/irsystem/models/init_tfidf.py
@@ -24,35 +24,33 @@ def init_tfidf_vectorizer(path_to_vocab, path_to_idf, path_to_tfidf):
 
 
 # sets global variables so we don't have to recalculate
-df = pd.read_csv('./data/so_rust.csv')
+so_df = pd.read_csv('./data/so_rust.csv')
 
-language_tokenizer = Tokenizer.from_file('./data/language-tokenizer.json')
-code_tokenizer = Tokenizer.from_file('./data/rust-tokenizer.json')
+so_language_tokenizer = Tokenizer.from_file('./data/language-tokenizer.json')
+so_code_tokenizer = Tokenizer.from_file('./data/rust-tokenizer.json')
 
 # Load the body Tfidf vocab counts and the idf array
-language_vectorizer, language_tf_idf = init_tfidf_vectorizer(
+so_language_vectorizer, so_language_tf_idf = init_tfidf_vectorizer(
     './data/rust_qa_body/vocab.json',
     './data/rust_qa_body/idf.npy',
     './data/rust_qa_body/tf_idf.npz'
 )
 
-code_vectorizer, code_tf_idf = init_tfidf_vectorizer(
+so_code_vectorizer, so_code_tf_idf = init_tfidf_vectorizer(
     './data/rust_qa_code/vocab.json',
     './data/rust_qa_code/idf.npy',
     './data/rust_qa_code/tf_idf.npz'
 )
 
 # TODO: Determine if this is useful
-combined_vectorizer, combined_tf_idf = init_tfidf_vectorizer(
+so_combined_vectorizer, so_combined_tf_idf = init_tfidf_vectorizer(
     './data/rust_qa_combined/vocab.json',
     './data/rust_qa_combined/idf.npy',
     './data/rust_qa_combined/tf_idf.npz'
 )
 
 # for each question, maps qid => actual question query
-qid_to_question = dict(zip(df.q_id, df.q_title))
+qid_to_question = dict(zip(so_df.q_id, so_df.q_title))
 
 # maps index of question title (0,1,2...) => actual question id
-idx_to_qid = dict(zip([i for i in range(len(df))], df.q_id))
-
-# language_tokenizer, a_vectorizer, a_tfidf, qid_to_question, idx_to_qid = get_tfidf()
+idx_to_qid = dict(zip([i for i in range(len(so_df))], so_df.q_id))
